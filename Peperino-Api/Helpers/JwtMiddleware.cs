36a7b4666a4824f61d4d38281237a37a7c6c1394
bifi <dennis.kaminski95@gmail.com>
2022-03-24 21:41:39 +0100

    UserContext caching

--- Peperino-Api/Helpers/JwtMiddleware.cs
+++ Peperino-Api/Helpers/JwtMiddleware.cs
@@ -1,12 +1,15 @@
 ﻿using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 using Peperino_Api.Services;
+using System.Collections.Concurrent;
 
 namespace Peperino_Api.Helpers
 {
     public class JwtMiddleware
     {
         public const string USER_CONTEXT = "USER_CONTEXT";
+
+        private static readonly ConcurrentDictionary<string, UserContext> _userContexts = new ConcurrentDictionary<string, UserContext>();
 
         private readonly RequestDelegate _next;
 
@@ -35,13 +38,30 @@ namespace Peperino_Api.Helpers
 
                 if (result is not null)
                 {
-                    var externalId = result.Uid;
+                    // Get user context from cache
+                    if (_userContexts.TryGetValue(token, out UserContext? userContext))
+                    {
+                        if (userContext is not null)
+                        {
+                            context.Items.Add(USER_CONTEXT, userContext);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        // Create new user context
+                        var externalId = result.Uid;
 
-                    var firebaseUser = await FirebaseAuth.GetAuth(firebase).GetUserAsync(externalId);
-                    var user = await userService.GetByExternalId(externalId);
+                        var firebaseUser = await FirebaseAuth.GetAuth(firebase).GetUserAsync(externalId);
+                        var user = await userService.GetByExternalId(externalId);
 
-                    UserContext userContext = new() { FirebaseUser = firebaseUser, PeperinoUser = user };
-                    context.Items.Add(USER_CONTEXT, userContext);
+                        UserContext newUserContext = new() { FirebaseUser = firebaseUser, PeperinoUser = user };
+
+                        if (_userContexts.TryAdd(token, newUserContext))
+                        {
+                            context.Items.Add(USER_CONTEXT, newUserContext);
+                        }
+                    }
                 }
             }
             catch
